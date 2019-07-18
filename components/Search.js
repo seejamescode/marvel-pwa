d329501b3767e8ae14fdb4c17570635afab85639
seejamescode <james@seejamescode.com>
2019-07-18 09:35:04 -0500

    style(search): less spacing on mobile

--- components/Search.js
+++ components/Search.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export default styled.section`
   display: grid;
   grid-gap: var(--padding);
-  margin: calc(6 * var(--padding)) auto;
+  margin: calc(2 * var(--padding)) auto;
   width: calc(100% - 2 * var(--padding));
+
+  @media only screen and (min-width: 720px) {
+    margin: calc(6 * var(--padding)) auto;
+  }
 `;
